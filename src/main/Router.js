b02bcbd0c08d47fed534669554702848dc8c2920
TonyK1109 <ngominhkhoi2014@gmail.com>
2021-06-26 23:06:56 +0700

    fix conflict

--- src/main/Router.js
+++ src/main/Router.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 
-module.exports = ({ }) => {
+module.exports = ({ authRoutes, customerRoutes }) => {
   const router = express.Router();
   router.use(express.static("public"));
   router.use(cors({ exposedHeaders: "auth-token" }));
   router.use(express.json());
   router.use(express.urlencoded({ extended: true }));
 
+  router.use("/auth", authRoutes);
   router.use("/customer", customerRoutes);
   return router;
 };
