fe61e8926822cc07d2401612b0db98f28e6139a6
Di <di@Minhminh.local>
2021-06-26 15:54:47 +0700

    Create table customer and a dummy customer

---
+++ src/models/customer_model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const customerSchema = mongoose.Schema({
+  email: {
+    type: String,
+    require: true,
+  },
+  name: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  phone: {
+    type: String,
+    require: true,
+  },
+  is_host: {
+    type: Boolean,
+    require: true,
+    default: false,
+  },
+  payment_number: {
+    type: String,
+    default: null,
+  },
+  ci: {
+    type: String,
+    default: null,
+  },
+});
+
+module.exports = mongoose.model("Customer", customerSchema);
