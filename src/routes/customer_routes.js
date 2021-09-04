8dd223018ecd43fd6e804c757a1aa67868c9db37
Di <di@Minhminh.local>
2021-09-04 14:31:00 +0700

    Remove token checking from customer view

--- src/routes/customer_routes.js
+++ src/routes/customer_routes.js
@@ -3,10 +3,10 @@ const express = require("express");
 module.exports = ({ customerController, authentication }) => {
   const router = express.Router();
 
-  router.get("/:id", authentication.verify, customerController.show);
+  router.get("/:id", customerController.show);
   router.get("/", customerController.index);
   router.put("/:id", authentication.verify, customerController.update);
-  router.get("/:id/rooms", authentication.verify, customerController.rooms);
+  router.get("/:id/rooms", customerController.rooms);
   router.get("/:id/recommended", authentication.verify, customerController.recommendedRooms);
 
   return router;
--- src/routes/order_routes.js
+++ src/routes/order_routes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 
 module.exports = ({ orderController, authentication }) => {
-    const router = express.Router();
+  const router = express.Router();
 
-    router.post("/", orderController.create);
-    router.get("/host/:id", authentication.verify, orderController.index);
-    router.get("/customer/:id", authentication.verify, orderController.indexCustomer);
-    router.get("/:id", authentication.verify, orderController.show);
-    router.put("/:id", authentication.verify, orderController.update);
+  router.post("/", orderController.create);
+  router.get("/host/:id", authentication.verify, orderController.index);
+  router.get("/customer/:id", authentication.verify, orderController.indexCustomer);
+  router.get("/:id", authentication.verify, orderController.show);
+  router.put("/:id", authentication.verify, orderController.update);
 
-    return router;
+  return router;
 };
